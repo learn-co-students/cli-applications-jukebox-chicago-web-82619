ba068bcdf3d5b795b9f396febfb05b68847600aa
Aaron Blum <blumaa@gmail.com>
2019-08-06 21:14:29 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -13,5 +13,5 @@ songs = [
 
 
 def help
-  puts "I accept the following commands:\n- help : displays this help message\n-list : "
+  puts "I accept the following commands:\n- help : displays this help message\n-list : displays a list of songs you can play\n- play : lets you choose a song to play\n- exit : exits this program"
 end
